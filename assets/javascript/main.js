1f9eef43a5c5a784b25dbe809c77c7c78cdf3695
aaliaga-casa <a.aliaga.val@gmail.com>
2017-07-02 02:24:28 -0300

    iniciando perfil

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,23 @@
-main.js
+function guardarDatos(){
+	localStorage.nombre=document.getElementById("nombre").value;
+	localStorage.password=document.getElementById("password").value;
+
+	if((localStorage.nombre.length == 0) && (localStorage.password.length == 0)){
+
+	alert("Debes ingresar tu nombre y tu password")
+	}
+
+}
+
+function recuperarDatos(){
+	if((localStorage.nombre != undefined) && (localStorage.password != undefined)){
+		document.getElementById("datos").innerHTML = 
+		"Nombre: " + localStorage.nombre + "<br/> Password: " + localStorage.password;
+	}else if((localStorage.nombre.length == 0) && (localStorage.password.length == 0)){
+
+	alert("Debes ingresar tu nombre y tu password")
+	}else{
+
+		document.getElementById("datos").innerHTML = "No has introducido tu nombre y tu password";
+	}
+}
