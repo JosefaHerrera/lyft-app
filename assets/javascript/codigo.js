603a29490a239fc5526f344af7b1bfd5d0b85807
JosefaHerrera <josefa@linuxero.cl>
2017-07-03 13:06:05 -0400

    finalizando

--- assets/javascript/codigo.js
+++ assets/javascript/codigo.js
@@ -1,3 +1,16 @@
- var random = ("LAB-" + (Math.floor(Math.random() * 899 + 100)));
- alert(random);
+$(document).ready(function() {
+    var codigoAleatorio = ((Math.floor(Math.random() * 999 + 100)));
+    alert("LAB-"+ codigoAleatorio);
 
+    localStorage.aleatorio = codigoAleatorio;
+
+    $("#enviar-code").click(function() {
+        var code = $("#num-codigo").val();
+        if (code == localStorage.aleatorio) {
+        	alert("Código ingresado");
+             window.location.href = 'signup2.html'
+        } else {
+            alert("Código incorrecto");
+        }
+    });
+});
