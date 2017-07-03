7b9237e4c95c270fc42315145fab370cc3380655
Melissa <melissa.pacheco.martinez@gmail.com>
2017-07-03 13:22:24 -0400

    show flag

--- assets/javascript/mainphone.js
+++ assets/javascript/mainphone.js
@@ -1,12 +1,25 @@
 $(document).ready(function() {
+
+
+    $("#mex").click(function(event){
+    event.preventDefault();
+    $(this).unbind(event);
+
+    $("#showflag").attr("src", "assets/img/flags/mexico.png");  // o  $(this).attr("src", nuevaImagen);
+
+});
+
+
     $("#next-button").hide();
 
     $('.required').on('keyup', function (){                    
            ToggleSubmitButton();
     }); 
+
 });
 
 function ToggleSubmitButton(){
+
     $("#next-button").hide();
     var getRequiredLength = $('.required').length;
     var nonempty = $('.required').filter(function(){
